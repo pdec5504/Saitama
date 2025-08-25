a23232224e006aac97ee4c0de475aa7a2a048b07
Pedro Dec <pdec5504@gmail.com>
2025-08-25 20:15:34 -0300

    feat: integrate RabbitMQ for event publishing and consumption in analysis service

--- backend/analysis/index.js
+++ backend/analysis/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
+// const axios = require('axios');
+const amqp = require('amqplib');
 
 const app = express();
 app.use(cors());
@@ -16,18 +19,12 @@ const analyseAndClassify = async (routineId) => {
         return;
     } 
 
-    // calculate the average of reps
-    // let repsSum = 0;
-    // for(const ex of routine.exercises){
-    //     repsSum += parseInt(ex.reps, 10);
-    // }
-
     const reps = routine.exercises.map(ex => parseInt(ex.reps, 10));
 
     const minReps = Math.min(...reps);
     const maxReps = Math.max(...reps);
 
-    let classification = "General Training"; //defaul
+    let classification = "General Training"; //default
 
     if(minReps <= 6 && maxReps >= 8){
         classification = "Hybrid Training (Strength/ Hypertrophy)";
@@ -46,36 +43,49 @@ const analyseAndClassify = async (routineId) => {
             classification = "Hybrid Training (Strength/ Hypertrophy)";
         }
     }
+
+
     console.log(`${routineId} Routine Analyses: [${reps.join(', ')}] reps -> Classification: ${classification}`);
 
-
-    // routine.exercises.forEach(ex => {
-    //     repsSum += parseInt(ex.reps, 10)
-    // })
-    // const repsAvg = repsSum / routine.exercises.length;
-
-    // if(repsAvg <= 6){
-    //     classification = "Strength Training"
-    // } else if(repsAvg >= 8 && repsAvg <= 12){
-    //     classification = "Hypertrophy Training"
-    // } else if(repsAvg > 12){
-    //     classification = "Resistance Training"
-    // } else{ // 6<avg<8
-    //     classification = "Hybrid Training (Strength/Hypertrophy)";
-    // }
+    const rabbitMQUrl = `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:5672`;
 
     try{
-        await axios.post("http://localhost:10000/events", {
-            type: "RoutineAnalyzed",
-            data:{
+        const connection = await amqp.connect(rabbitMQUrl);
+        const channel = await connection.createChannel();
+        const exchange = "event_exchange";
+
+        const analysisEvent = {
+            type: 'RoutineAnalyzed',
+            data: {
                 routineId: routine.id,
                 classification: classification
             }
-        });
+        };
+
+        await channel.assertExchange(exchange, 'fanout', { durable:false });
+        channel.publish(exchange, '', Buffer.from(JSON.stringify(analysisEvent)));
+        console.log(`Publisher (Analysis): Event [${analysisEvent.type}] published.`)
+
+        await channel.close()
+        await connection.close();
     } catch(error){
         console.error("Error to send analysis event: ", error.message);
     }
 };
+
+
+//     try{
+//         await axios.post("http://localhost:10000/events", {
+//             type: "RoutineAnalyzed",
+//             data:{
+//                 routineId: routine.id,
+//                 classification: classification
+//             }
+//         });
+//     } catch(error){
+//         console.error("Error to send analysis event: ", error.message);
+//     }
+// };
 
 const functions = {
 
@@ -94,17 +104,49 @@ const functions = {
 
 };
 
-app.post("/events", (req, res) => {
-    const { type, data } = req.body;
-    console.log("Event received (Analysis):", type);
+// old logic
+// app.post("/events", (req, res) => {
+//     const { type, data } = req.body;
+//     console.log("Event received (Analysis):", type);
 
-    if(functions[type]){
-        functions[type](data);
+//     if(functions[type]){
+//         functions[type](data);
+//     }
+
+//     res.status(200).send({ status: "OK" });
+// });
+
+async function startConsumer(){
+    const rabbitMQUrl = `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:5672`;
+    try{
+        const connection = await amqp.connect(rabbitMQUrl);
+        const channel = await connection.createChannel();
+        const exchange = "event_exchange";
+
+        await channel.assertExchange(exchange, 'fanout', { durable:false });
+        const q = await channel.assertQueue('analysis_event', { durable: true });
+        console.log(`Consumer (Analysis): Waiting for messages in queue: ${q.queue}`);
+
+        await channel.bindQueue(q.queue, exchange, '');
+
+        channel.consume(q.queue, (msg) => {
+            if(msg.content){
+                const event = JSON.parse(msg.content.toString());
+                console.log(`Consumer (Analysis): Event received - ${event.type}`, event.type);
+
+                if(functions[event.type]){
+                    functions[event.type](event.data);
+                }
+                channel.ack(msg);
+            }
+        });
+    }catch(error){
+        console.error("Error in Consumer (Analysis):", error.message);
     }
 
-    res.status(200).send({ status: "OK" });
-});
+}
 
 app.listen(7000, () => {
     console.log("Analysis server is running on port 7000.");
+    startConsumer();
 })
