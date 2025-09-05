b43fbdef9ebd1b830afa30fba73f5f84e85c2500
Pedro Dec <pdec5504@gmail.com>
2025-09-05 17:30:33 -0300

    feat: enable routine drag-and-drop only in edit mode

--- frontend/src/components/SortableRoutineCard.jsx
+++ frontend/src/components/SortableRoutineCard.jsx
@@ -2,19 +2,22 @@ import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import RoutineCard from './RoutineCard';
 
-export function SortableRoutineCard(props) {
+export function SortableRoutineCard({ isEditMode, ...props}) {
     const {
         attributes,
         listeners,
         setNodeRef,
         transform,
         transition,
-    } = useSortable({ id: props.id });
+    } = useSortable({ id: props.id, disabled: !isEditMode, });
 
     const style = {
         transform: CSS.Transform.toString(transform),
         transition,
+        cursor: isEditMode ? "grab" : "default",
     };
+
+    const dragListeners = isEditMode ? listeners : {};
 
     return(
         <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
